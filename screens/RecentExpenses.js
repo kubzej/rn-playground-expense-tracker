a2383b2ef4bfe6f5230aeaa50550e90d845d06de
Jakub Mares <jakub.mares@heidelbergcement.com>
2022-12-09 14:15:01 +0100

    fetch data and return them formatted

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,20 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 import { ExpensesContext } from '../store/expenses-context';
 import { getDateMinusDays } from '../util/date';
+import { fetchExpenses } from '../util/http';
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
@@ -17,8 +26,8 @@ function RecentExpenses() {
   return (
     <ExpensesOutput
       expenses={recentExpenses}
-      expensesPeriod="Last 7 Days"
-      fallbackText="No expenses registered for the last 7 days."
+      expensesPeriod='Last 7 Days'
+      fallbackText='No expenses registered for the last 7 days.'
     />
   );
 }
